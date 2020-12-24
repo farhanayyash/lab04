a848c2f4fb007aae2c19a22b6041f4b2dcea6aa5
farhanayyash <farhanayyash16@yahoo.com>
2020-12-24 15:49:44 +0200

    final

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ console.log("turkey");
 console.log("This page has been built by Farhan Ayyash");
 var x;
 x = prompt('what is you name?');
+while (x == "" || x == null){
+  x = prompt('what is you name?');
+}
 var today= new Date();
 var hourNow = today.getHours();
 var greeting;
@@ -16,6 +19,17 @@ greeting = 'Good morning!';
 greeting = 'Welcome! ' ;
 }
 document.write('<p>Hello <b>'+ x +'</b>, '+ greeting + '</p>')
+
 function myFunction() {
     alert(x +" will go to another page, Good Bye!");
   }
+var c = ""
+function myFunction2() {
+  y = prompt('How many photos do you want?');
+  for (i =0 ; i < y ; i++){
+    console.log(i)
+    c =c + '<img style="width:500px; height:auto ;margin:0 0 0 200px;" src="https://www.conexioconsulting.com/wp-content/uploads/2017/11/17-INTERESTING-FACTS-ABOUT-TURKEY.jpg" alt="photo about turkey">'+'<h1 style="margin:-50px 0 0 900px;">'+(i+1)+'</h1>'+'<br>'  ;  
+  }
+  var result_display = document.getElementById('result');
+  result_display.innerHTML = c;
+}
